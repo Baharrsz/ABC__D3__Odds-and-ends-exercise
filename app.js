e5a56bff4608b0a5890cecd3a51d12e23bba5eba
Bahar Raiszadeh <Bahar@Bahars-MBP-2.vc.shawcable.net>
2021-03-02 18:23:22 -0800

    Added data

---
+++ app.js
@@ -0,0 +1,5 @@
+d3.csv("./data/co2/API_EN.ATM.CO2E.KT_DS2_en_csv_v2.csv", d => console.log("co2", d))
+d3.csv("./data/methane/API_EN.ATM.METH.KT.CE_DS2_en_csv_v2.csv", d => console.log("methane", d))
+d3.csv("./data/population/API_SP.POP.TOTL_DS2_en_csv_v2.csv", d => console.log("population", d))
+d3.csv("./data/renewable/API_EG.FEC.RNEW.ZS_DS2_en_csv_v2.csv", d => console.log("renewable", d))
+d3.csv("./data/urban_population/API_SP.URB.TOTL_DS2_en_csv_v2.csv", d => console.log("urban", d))
