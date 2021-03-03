a389c41e6ff3e1f5b4efe243cf15ba026a5c5304
Bahar Raiszadeh <Bahar@Bahars-MBP-2.vc.shawcable.net>
2021-03-03 14:44:13 -0800

    Put data requests on a queue. Added formatter for individual data requests

--- app.js
+++ app.js
@@ -1,5 +1,72 @@
-d3.csv("./data/co2/API_EN.ATM.CO2E.KT_DS2_en_csv_v2.csv", d => console.log("co2", d))
-d3.csv("./data/methane/API_EN.ATM.METH.KT.CE_DS2_en_csv_v2.csv", d => console.log("methane", d))
-d3.csv("./data/population/API_SP.POP.TOTL_DS2_en_csv_v2.csv", d => console.log("population", d))
-d3.csv("./data/renewable/API_EG.FEC.RNEW.ZS_DS2_en_csv_v2.csv", d => console.log("renewable", d))
-d3.csv("./data/urban_population/API_SP.URB.TOTL_DS2_en_csv_v2.csv", d => console.log("urban", d))
+d3.queue()
+    .defer(d3.csv,"./data/co2/API_EN.ATM.CO2E.KT_DS2_en_csv_v2.csv", formatter)
+    .defer(d3.csv,"./data/methane/API_EN.ATM.METH.KT.CE_DS2_en_csv_v2.csv", formatter)
+    .defer(d3.csv,"./data/population/API_SP.POP.TOTL_DS2_en_csv_v2.csv", formatter)
+    .defer(d3.csv,"./data/renewable/API_EG.FEC.RNEW.ZS_DS2_en_csv_v2.csv", formatter)
+    .defer(d3.csv,"./data/urban_population/API_SP.URB.TOTL_DS2_en_csv_v2.csv", formatter)
+    .awaitAll((error, data) => {
+        if (error) console.log(error);
+        console.log(data);
+       
+    })
+
+
+function formatter(row, i, headers) {
+     var invalidRows = [
+    "Arab World", 
+    "Central Europe and the Baltics",
+    "Caribbean small states",
+    "East Asia & Pacific (excluding high income)",
+    "Early-demographic dividend",
+    "East Asia & Pacific",
+    "Europe & Central Asia (excluding high income)",
+    "Europe & Central Asia",
+    "Euro area",
+    "European Union",
+    "Fragile and conflict affected situations",
+    "High income",
+    "Heavily indebted poor countries (HIPC)",
+    "IBRD only",
+    "IDA & IBRD total",
+    "IDA total",
+    "IDA blend",
+    "IDA only",
+    "Not classified",
+    "Latin America & Caribbean (excluding high income)",
+    "Latin America & Caribbean",
+    "Least developed countries: UN classification",
+    "Low income",
+    "Lower middle income",
+    "Low & middle income",
+    "Late-demographic dividend",
+    "Middle East & North Africa",
+    "Middle income",
+    "Middle East & North Africa (excluding high income)",
+    "North America",
+    "OECD members",
+    "Other small states",
+    "Pre-demographic dividend",
+    "Pacific island small states",
+    "Post-demographic dividend",
+    "Sub-Saharan Africa (excluding high income)",
+    "Sub-Saharan Africa",
+    "Small states",
+    "East Asia & Pacific (IDA & IBRD countries)",
+    "Europe & Central Asia (IDA & IBRD countries)",
+    "Latin America & the Caribbean (IDA & IBRD countries)",
+    "Middle East & North Africa (IDA & IBRD countries)",
+    "South Asia (IDA & IBRD)",
+    "Sub-Saharan Africa (IDA & IBRD countries)",
+    "Upper middle income",
+    "World"
+  ];
+  if (invalidRows.indexOf(row["Country Name"]) > -1) return;
+  let result = {
+      countryName: row["Country Name"], 
+      indicator: row["Indicator Name"]}
+  headers.forEach(key => {
+      if (parseInt(key)) result[key] = +row[key]  || null;
+  })
+  return result;
+}
+
